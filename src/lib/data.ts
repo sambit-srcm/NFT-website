7e50d9697470937e45818e5f1e8c26a4ba43ca73
Sambit Mohanty <sambitsrcm@gmail.com>
2026-09-09 15:07:50 +0530

    fix: Update The Orbitians description
    src/lib/data.ts: replaced the placeholder description text with the
    real copy for the collection.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -306,7 +306,8 @@ export const NFT_DETAIL: NftDetail = {
   description: [
     "The Orbitians is a collection of 10,000 unique NFTs on the Ethereum blockchain.",
     "There are all sorts of beings in the NFT Universe. The most advanced and friendly of the bunch are Orbitians.",
-    "They live in a metal space machine, high up in the sky and only have one law: be kind to each other.",
+    "They live in a metal space machines, high up in the sky and only have one foot on Earth.",
+    "These Orbitians are a peaceful race, but they have been at war with a group of invaders for many generations. The invaders are called Upside-Downs, because of their inverted bodies that live on the ground, yet do not know any other way to be. Upside-Downs believe that they will be able to win this war if they could only get an eye into Orbitian territory, so they've taken to make human beings their target.",
   ],
   tags: ["ANIMATION", "ILLUSTRATION", "MOON"],
   price: 1.63,
